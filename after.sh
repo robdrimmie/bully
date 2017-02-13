fb0c8ae538e1c1dedb2305de9c35e17b8a8ecd94
Gord Tanner <gord@dozr.com>
2017-02-13 14:18:08 -0500

    add a better "finished" msg

--- after.sh
+++ after.sh
@@ -2,3 +2,13 @@ cd /vagrant
 cp -n .env.example .env
 php artisan migrate
 php artisan db:seed
+
+echo 
+echo '----------------------------------------------------------------------'
+echo '        ____'
+echo '    \  i | o|'
+echo '    |>#######'
+echo '    /(_______)'
+echo 'Your server is configured to run on' $(cat Homestead.yaml | grep ip)
+echo '----------------------------------------------------------------------'
+
