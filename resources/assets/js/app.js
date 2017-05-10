46bcb483f9553ce62427fa3b94a8eed637153890
dmck0y <dmckoy85@gmail.com>
2017-05-10 15:34:42 -0400

    stop using createClass

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -2,43 +2,35 @@ import React from 'react';
 import { render } from 'react-dom';
 import ImageGrid from './components/ImageGrid.jsx';
 
-const { string } = React.PropTypes;
-
 const reactHook = document.querySelector('[data-js="react-hook"]');
 
-const InstructionComponent = React.createClass({
-  propTypes: {
-    name: string
-  },
-
-  render () {
-    return (
-      <div className="pt4"> 
-        <header className="ph4">
-          <img src="/images/dozr_logo.svg" />
-          <div className="pt4">
-            <h1 className="f3 fw5 ttu oswald">Welcome to The Dozr challenge</h1>
-            <ol className="pa0">
-              <li className="pv1">Use the design documents as your guide for building a grid of equipment listings. <a target="_blank" href="https://drive.google.com/drive/folders/0B04K4CL8KavAdGNkUHFkdUVESHc?usp=sharing">here</a></li>
-              <li className="pv1">Each listing should link to a detailed view of the listing.</li>
-              <li className="pv1">All views need to responsive for mobile and desktop screens.</li>
-              <li className="pv1">You can fetch all listing data from /api/equipment and details data at /api/equipment/[$id].</li>
-              <li className="pv1">Use the included tachyons css library for styling your components. <a target="_blank" href="https://github.com/tachyons-css/tachyons">You can visit the tachyons github to get more info about this library.</a></li>
-            </ol>
-          </div>
-        </header>
-        <div className="pt4 ph4">
-          <h3 className="fw1 ttu oswald">{`What you'll be building`}</h3>
-          <ImageGrid links={['listview.png', 'listing.png', 'mobile.png', 'guide.png']} />
+const InstructionComponent = () => {
+  return (
+    <div className="pt4">
+      <header className="ph4">
+        <img src="/images/dozr_logo.svg" />
+        <div className="pt4">
+          <h1 className="f3 fw5 ttu oswald">Welcome to The Dozr challenge</h1>
+          <ol className="pa0">
+            <li className="pv1">Use the design documents as your guide for building a grid of equipment listings. <a target="_blank" href="https://drive.google.com/drive/folders/0B04K4CL8KavAdGNkUHFkdUVESHc?usp=sharing">here</a></li>
+            <li className="pv1">Each listing should link to a detailed view of the listing.</li>
+            <li className="pv1">All views need to responsive for mobile and desktop screens.</li>
+            <li className="pv1">You can fetch all listing data from /api/equipment and details data at /api/equipment/[$id].</li>
+            <li className="pv1">Use the included tachyons css library for styling your components. <a target="_blank" href="https://github.com/tachyons-css/tachyons">You can visit the tachyons github to get more info about this library.</a></li>
+          </ol>
         </div>
-        <div className="pa4 bg-light-grey">
-          <span className="bg-gold br1 dib pa3">
-            <a className="link link-underline white pa3" target="_blank" href="https://drive.google.com/drive/folders/0B04K4CL8KavAdGNkUHFkdUVESHc?usp=sharing">Download Design Files</a>
-          </span>
-        </div>
+      </header>
+      <div className="pt4 ph4">
+        <h3 className="fw1 ttu oswald">{`What you'll be building`}</h3>
+        <ImageGrid links={['listview.png', 'listing.png', 'mobile.png', 'guide.png']} />
       </div>
-    );
-  }
-});
+      <div className="pa4 bg-light-grey">
+        <span className="bg-gold br1 dib pa3">
+          <a className="link link-underline white pa3" target="_blank" href="https://drive.google.com/drive/folders/0B04K4CL8KavAdGNkUHFkdUVESHc?usp=sharing">Download Design Files</a>
+        </span>
+      </div>
+    </div>
+  );
+}
 
 render(<InstructionComponent name="Friend" />, reactHook);
--- resources/assets/js/components/ImageGrid.jsx
+++ resources/assets/js/components/ImageGrid.jsx
@@ -2,26 +2,24 @@ import React from 'react';
 
 const { array } = React.PropTypes;
 
-const ImageGrid = React.createClass({
-  propTypes: {
-    links: array
-  },
-
-  render () {
-    const images = this.props.links.map((link, i) => {
-      return (
-        <div key={i} className="fl-ns w-50-ns pa3">
-          <img src={`/images/${link}`} />
-        </div>
-      );
-    });
-
+const ImageGrid = ({ links }) => {
+  const images = links.map((link, i) => {
     return (
-      <div className="cf">
-        {images}
+      <div key={i} className="fl-ns w-50-ns pa3">
+        <img src={`/images/${link}`} />
       </div>
     );
-  }
-});
+  });
+
+  return (
+    <div className="cf">
+      {images}
+    </div>
+  );
+};
+
+ImageGrid.propTypes = {
+  links: array
+};
 
 export default ImageGrid;
