5ceb468849f95c5b0dd69165f8688fbfc285b4f5
rmdrimmie <rob.drimmie@gmail.com>
2017-06-16 14:59:54 -0400

    Basic resource controller and tests.

---
+++ tests/Unit/Controllers/EquipmentControllerTest.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace Tests\Unit\Controllers;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class EquipmentControllerTest extends TestCase
+{
+    /**
+     * Test resource listing
+     *
+     * @return void
+     */
+    public function testIndex()
+    {
+        $this->assertTrue(false);
+    }
+
+    /**
+     * Test show the form for creating a new resource.
+     *
+     * @return void
+     */
+    public function testCreate()
+    {
+        $this->assertTrue(false);
+    }
+
+    /**
+     * Test storing a newly created resource in storage.
+     *
+     * @return void
+     */
+    public function testStore()
+    {
+        $this->assertTrue(false);
+    }
+
+    /**
+     * Test displaying the specified resource.
+     *
+     * @return void
+     */
+    public function show()
+    {
+        $this->assertTrue(false);
+    }
+
+    /**
+     * Test showing the form for editing the specified resource.
+     *
+     * @return void
+     */
+    public function edit()
+    {
+        $this->assertTrue(false);
+    }
+
+    /**
+     * Test updating the specified resource in storage.
+     *
+     * @return void
+     */
+    public function testUpdate()
+    {
+        $this->assertTrue(false);
+    }
+
+    /**
+     * Test removing the specified resource from storage.
+     *
+     * @return void
+     */
+    public function testDestroy()
+    {
+        $this->assertTrue(false);
+    }
+}
